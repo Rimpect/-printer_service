322e6812c46f1386fff58220f3aed53b89000d04
Rimpect <gitlaborhub@gmail.com>
2025-05-27 03:05:01 +0300

    основа под токены

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const pool = require('./src/config/database');
+const cookieParser = require('cookie-parser');
+const authRouter = require('./src/routers/auth');
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use('/auth', authRouter);
 
 pool.query('SELECT NOW()', (err, res) => {
   if(err) {
@@ -10,8 +15,11 @@ pool.query('SELECT NOW()', (err, res) => {
     console.log('Connected to the database:', res.rows);
   }
 });
+app.get('/profile', require('./src/middleware/auth.js'), (req, res) => {
+  res.json({ message: `User ID: ${req.userId}` });
+});
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
