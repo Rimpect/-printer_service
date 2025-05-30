532dce1697fc1fb5ce6c26c4269b858794110ed6
Rimpect <gitlaborhub@gmail.com>
2025-05-30 19:23:38 +0300

    Hello

---
+++ src/components/Context/AuthContext.js
@@ -0,0 +1,21 @@
+import { createContext, useState, useEffect } from 'react';
+import axios from 'axios';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      setIsAuthenticated(true);
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
